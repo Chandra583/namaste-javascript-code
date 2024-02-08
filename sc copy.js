2d3d66e4a747faabc2f134df1cc055bde6fef9a3
Chandra583 <chandrashekhargawda2000@gmail.com>
2024-02-08 12:11:37 +0530

    steTimeOut

--- sc copy.js
+++ sc copy.js
@@ -12,11 +12,30 @@
 // });
 
 //Creating event listener
-function attahed() {
-  let count = 0;
-  document.getElementById("clickMe").addEventListener("click", function xyz() {
-    console.log("you clicked", ++count);
-  });
+// function attahed() {
+//   let count = 0;
+//   document.getElementById("clickMe").addEventListener("click", function xyz() {
+//     console.log("you clicked", ++count);
+//   });
+// }
+
+// attahed();
+
+///trust issue with setTimeout
+console.log("start");
+
+//using callback
+setTimeout(function cb() {
+  console.log("callback");
+}, 5000);
+console.log("end");
+
+//
+let startDate = new Date().getTime();
+let endDate = startDate;
+
+while (endDate < startDate + 10000) {
+  endDate = new Date().getTime();
 }
 
-attahed();
+console.log("while expire");
