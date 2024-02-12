81797848b4417ea8cc86bd251f10336bd34d1664
Chandra583 <chandrashekhargawda2000@gmail.com>
2024-02-12 10:07:49 +0530

    a

--- sc copy.js
+++ sc copy.js
@@ -42,17 +42,65 @@
 
 // //
 
-const arr = [3, 4, 5, 1];
+// const arr = [3, 4, 5, 1];
 
-const circle = function (arr) {
-  return Math.PI * arr * arr;
-};
+// const circle = function (arr) {
+//   return Math.PI * arr * arr;
+// };
 
-const caluculate = function (arr, logic) {
-  const result = [];
+// const caluculate = function (arr, logic) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(logic(arr[i]));
+//   }
+//   return result;
+// };
+// console.log(caluculate(arr, circle));
+
+// console.log(arr.)
+
+// console.log(arr.map(area));
+// const redius = [5, 4, 5, -3];
+// const arr = [4, 5, 6, 8];
+
+// const areaOfCircle = function (redius) {
+//   return Math.PI * redius * redius;
+// };
+
+// // console.log(arr.map(areaOfCircle));
+// // console.log(redius.map(areaOfCircle));
+
+// const toCaluculate = function (redius, areaOfCircle) {
+//   const output = [];
+//   for (let i = 0; i < redius.length; i++) {
+//     output.push(areaOfCircle(redius[i]));
+//   }
+//   return output;
+// };
+// console.log(toCaluculate(redius, areaOfCircle));
+
+// const arr = [5, 8, 9, 6, 1, 2];
+// function double(x) {
+//   return x % 2 === 0;
+// }
+
+// const output = arr.filter((x) => {
+//   return x < 4;
+// });
+// console.log(output);
+
+//reducer
+
+const arr = [1, 2, 3, 4, 5, 88, 5];
+
+function toFindMax(arr) {
+  let max = 0;
   for (let i = 0; i < arr.length; i++) {
-    result.push(logic(arr[i]));
+    if (arr[i] > max) {
+      max = arr[i];
+    }
   }
-  return result;
-};
-console.log(caluculate(arr, circle));
+  return max;
+}
+
+console.log(toFindMax(arr));
